ed29aaf8800ea60ab26c17243d02a4c903aa5411
Kozheb <vhazov95@yandex.ru>
2024-09-23 14:52:53 +0300

    v2

---
+++ ASCON_C#/lesson_1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите свою фамилию:");
+        string surname = Console.ReadLine();
+        Console.WriteLine("Введите свое имя:");
+        string name = Console.ReadLine();
+        Console.WriteLine("Введите свое отчество");
+        string patronymic = Console.ReadLine();
+        string text = $"Здравствуйте, {surname} {name} {patronymic}";
+        Console.WriteLine(text);
+    }
+}
