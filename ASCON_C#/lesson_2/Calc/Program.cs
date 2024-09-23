0e774d236dbd6cfdc0384084fe25a92c2f53d2bf
Kozheb <vhazov95@yandex.ru>
2024-09-23 15:49:43 +0300

    v1

---
+++ ASCON_C#/lesson_2/Calc/Program.cs
@@ -0,0 +1,36 @@
+﻿double result;
+Console.Write("Введите первое число:");
+double first = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите введите второе число:");
+double second = Convert.ToDouble(Console.ReadLine());
+Console.Write("Определите действие между числами (+, -, *, /, %):");
+string operation = Console.ReadLine();
+
+switch (operation)
+{
+    case "+":
+        result = first + second;
+        Console.WriteLine(result);
+        break;
+    case "-":
+        result = first - second;
+        Console.WriteLine(result);
+        break;
+    case "*":
+        result = first * second;
+        Console.WriteLine(result);
+        break;
+    case "/":
+        result = first / second;
+        Console.WriteLine(result);
+        break;
+    case "%":
+        result = first % second;
+        Console.WriteLine(result);
+        break;
+    default:
+        Console.WriteLine("Действие не поддерживается");
+        break;
+}
+
+Console.ReadKey();
